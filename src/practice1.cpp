32554208dfbb3fa3af5ffd10ac9012e840d88889
Jordan Smith <jordansmith@gmail.com>
2022-07-23 16:23:28 -0700

    Fixed tasks.json and edited readme

--- src/practice1.cpp
+++ src/practice1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <C:\Users\Jorda\Desktop\GitHub\include\glad\glad.h>
-#include <C:\Users\Jorda\Desktop\GitHub\include\GLFW\glfw3.h>
+#include <glad.h>
+#include <glfw3.h>
 
 const unsigned int WIND_WIDTH = 1600;
 const unsigned int WIND_HEIGHT = 900;
@@ -10,6 +10,17 @@ int main(){
 
     // glwf initialization and configuration process
     glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    return 0;
+
+    // glfw window creation
+    GLFWwindow *window = glfwCreateWindow(WIND_WIDTH, WIND_HEIGHT, "Practice", NULL, NULL);
+    if(window == NULL){
+        std::cout << "Failed to create GLFW window.\n";
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
 }
