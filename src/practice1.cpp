4c81f9a83837e7e1389a918c07f614c984020845
Jordan Smith <jordansmith@gmail.com>
2022-07-23 15:45:08 -0700

    Add GLFW and GLAD files for OpenGL

---
+++ src/practice1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <C:\Users\Jorda\Desktop\GitHub\include\glad\glad.h>
+#include <C:\Users\Jorda\Desktop\GitHub\include\GLFW\glfw3.h>
+
+const unsigned int WIND_WIDTH = 1600;
+const unsigned int WIND_HEIGHT = 900;
+
+int main(){
+
+    // glwf initialization and configuration process
+    glfwInit();
+
+    return 0;
+}
