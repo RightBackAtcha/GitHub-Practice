dbbf5f675088f874f4a59f7eb0b650019a309166
Jordan Smith <jordansmith@gmail.com>
2022-07-23 17:35:10 -0700

    Fixed task.json bug inhibiting running of code

---
+++ practice1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+const unsigned int WND_HEIGHT = 720;
+const unsigned int WND_WIDTH = 1280;
+
+int main(){
+
+}
