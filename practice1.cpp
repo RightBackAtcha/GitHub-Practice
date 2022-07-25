ef6b9ed8a02c3e80be39fab07f99c03784266291
Jordan Smith <jordansmith@gmail.com>
2022-07-25 00:40:45 -0700

    Going back to the basics

--- practice1.cpp
+++ practice1.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <vector>
-#include "SDL2/sdl.h"
-
-const unsigned int WND_HEIGHT = 720;
-const unsigned int WND_WIDTH = 1280;
+#include <string>
+#include <thread>
 
 int main(){
-    return 0;
+     return 0;
 }
