c53e77e36bd5247aa2c0291d155798f8b490dc84
Jordan Smith <jordansmith@gmail.com>
2022-07-26 00:47:30 -0700

    Added feature to open file when program ends

--- src/practice1.cpp
+++ src/practice1.cpp
@@ -2,17 +2,18 @@
 #include <fstream>
 #include <string>
 
-// Includes header file that I created
+// Includes header file that I created for different practice functions
 #include "practiceFuncs.hpp"
 
 
-int main(){
+int main(int argc, char *argv[]){
      std::string fileName;
 
      std::cout << "Name of file to be alphabetically sorted.\n";
      std::cin >> fileName;
      std::ofstream test("..\\" + fileName + ".txt", std::ios::app);
 
+     // Check header file for more information on this function
      jord::sortAlphabet(fileName);
 
      return 0;
--- src/practiceFuncs.hpp
+++ src/practiceFuncs.hpp
@@ -2,22 +2,24 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <windows.h>
 
 namespace jord{
-    // Function that sorts txt files alphabetically (sorts only by first letter of string)
-    int sortAlphabet(std::string fileName){ 
+    // Function that sorts txt files alphabetically (sorts only by first letter of string currently)
+    int sortAlphabet(std::string fileName){  
         int lineCount = 0;
         int ix = 0;
 
         std::string temp;
+        std::string exitPath = ("..\\" + fileName + "_output.txt");
         std::ifstream inputStrings;
         inputStrings.open("..\\" + fileName + ".txt", std::ios::app);
         std::ofstream outputStrings;
         outputStrings.open("..\\" + fileName + "_output.txt");
 
-        while(getline(inputStrings, temp)){
+        while(getline(inputStrings, temp)){ // Counts total amount of lines found in input file
             ++lineCount;
-        }
+        } 
         std::string strArray[lineCount];
         std::vector<int> strVector(lineCount);
         std::vector<int> usage(lineCount);
@@ -127,6 +129,8 @@ namespace jord{
                 }
             }
         }
+        outputStrings.close(); // After execution of function, opens output file
+        ShellExecuteA(NULL, "open", exitPath.c_str(), NULL, NULL, 0);
         return 0;
     }
 }
