38e35c35ce4d9356569b060313c76532a2f70570
Jordan Smith <jordansmith@gmail.com>
2022-08-05 23:33:26 -0700

    Fixed checkWinner function bug

--- src/tictactoe.cpp
+++ src/tictactoe.cpp
@@ -6,11 +6,17 @@
 
 using namespace std;
 
+// Function that outputs current game state after each turn
 void gameResponse(char spaces[]);
-bool checkWinner(char spaces[]);
+
+// Function that checks whether the player has won or not (outputs 1 for X and -1 for O)
+int checkWinner(char spaces[]);
+
+// Function used to generate random AI turn for singleplayer game
 void aiInput(char spaces[], default_random_engine);
 
-int main(){ // Simple game of tic-tac-toe made using C++
+// Simple game of tic-tac-toe made using C++
+int main(){
     int playerInput = 0;
     string player1, player2;
     int temp;
@@ -21,12 +27,13 @@ int main(){ // Simple game of tic-tac-toe made using C++
     random_device rnd;
     default_random_engine ai(rnd());
 
+    // Main loop function for tic tac toe game
     cout << "[1] Singleplayer\n[2] Multiplayer\n";
     cin >> temp;
-    if(temp == 1){
+    if(temp == 1){ // Singleplayer component of main game loop
         cout << "Player Name...(X)\n";
         cin >> player1;
-        while(turns < 9 || checkWinner(spaces) == false){
+        while(turns < 9 || checkWinner(spaces) == -1){
             cout << "What space would you like to take (1 - 9) " << player1 << "?\n";
             while(playerInput == 0){
                 cin >> playerInput;
@@ -46,34 +53,25 @@ int main(){ // Simple game of tic-tac-toe made using C++
             turns++;
             gameResponse(spaces);
 
-            if(checkWinner(spaces)){
-                cout << "3 in a row! You win!\n";
-                this_thread::sleep_for(chrono::milliseconds(3500));
-                return 1;
+            if(checkWinner(spaces) == -1){
+                cout << "You Lose...\n";
+                this_thread::sleep_for(chrono::milliseconds(3500));                return -1;
             }
         }
-        if(!checkWinner(spaces)){
-            cout << "You lose!\n";
+        if(checkWinner(spaces) == 1){
+            cout << "3 in a row! You Win!\n";
             this_thread::sleep_for(chrono::milliseconds(3500));
-            return -1;
+            return 1;
         } else
             cout << "Tie Game!";
             this_thread::sleep_for(chrono::milliseconds(3500));
             return 0;
     }
-    else if(temp == 2){
+    else if(temp == 2){ // Multiplayer component of main game loop
         cout << "Player 1 Name... (X)\n";
         cin >> player1;
         cout << "Player 2 Name... (O)\n";
         cin >> player2;
-    }
-    if(checkWinner(spaces)){
-        cout << "You Win!";
-        return 0;
-    }
-    else if(checkWinner(spaces)){
-        cout << "You Lose!";
-        return 0;
     }
 
     return 0;
@@ -92,57 +90,56 @@ void gameResponse(char spaces[]){
     cout << spaces[6]; cout << " | "; cout << spaces[7]; cout << " | "; cout << spaces[8];
     cout << "\n   |   |   \n"; 
 }
-bool checkWinner(char spaces[]){
+int checkWinner(char spaces[]){
     if(spaces[0] == 'X' && spaces[1] == 'X' && spaces[2] == 'X'){
-        return true;
+        return 1;
     }
     else if(spaces[0] == 'O' && spaces[1] == 'O' && spaces[2] == 'O'){
-        return false;
+        return -1;
     }
     else if(spaces[3] == 'X' && spaces[4] == 'X' && spaces[5] == 'X'){
-        return true;
+        return 1;
     }
     else if(spaces[3] == 'O' && spaces[4] == 'O' && spaces[5] == 'O'){
-        return false;
+        return -1;
     }
     else if(spaces[6] == 'X' && spaces[7] == 'X' && spaces[8] == 'X'){
-        return true;
+        return 1;
     }
     else if(spaces[6] == 'O' && spaces[7] == 'O' && spaces[8] == 'O'){
-        return false;
+        return -1;
     }
     else if(spaces[1] == 'X' && spaces[3] == 'X' && spaces[6] == 'X'){
-        return true;
+        return 1;
     } 
     else if(spaces[1] == 'O' && spaces[3] == 'O' && spaces[6] == 'O'){
-        return false;
+        return -1;
     } 
     else if(spaces[1] == 'X' && spaces[4] == 'X' && spaces[7] == 'X'){
-        return true;
+        return 1;
     }
     else if(spaces[1] == 'O' && spaces[4] == 'O' && spaces[7] == 'O'){
-        return false;
+        return -1;
     }
     else if(spaces[2] == 'X' && spaces[5] == 'X' && spaces[8] == 'X'){
-        return true;
+        return 1;
     } 
     else if(spaces[2] == 'O' && spaces[5] == 'O' && spaces[8] == 'O'){
-        return false;
+        return -1;
     } 
     else if(spaces[0] == 'X' && spaces[4] == 'X' && spaces[8] == 'X'){
-        return true;
+        return 1;
     }
     else if(spaces[0] == 'O' && spaces[4] == 'O' && spaces[8] == 'O'){
-        return false;
+        return -1;
     }
     else if(spaces[2] == 'X' && spaces[4] == 'X' && spaces[6] == 'X'){
-        return true;
+        return 1;
     }
     else if(spaces[2] == 'O' && spaces[4] == 'O' && spaces[6] == 'O'){
-        return false;
+        return -1;
     }
-
-    return NULL;
+    return 0;
 }
 void aiInput(char spaces[], default_random_engine ai){
     uniform_int_distribution<int> gen(0, 8);
